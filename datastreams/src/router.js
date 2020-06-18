87ec2c114d36b391b0b92066e0a47d0b499dce7d
Joshua3011 <j.vangompel@student.fontys.nl>
2020-06-18 22:39:28 +0200

    Setup first 4 components dashboard + created Messages page

--- datastreams/src/router.js
+++ datastreams/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Berichten from './views/Berichten.vue'
 import Dashboard from './views/Dashboard.vue'
 import DocumentsPage from './views/DocumentsPage.vue'
 import SendingPage from './views/SendingPage.vue'
@@ -9,7 +10,8 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/Dashboard', component: Dashboard },
   { path: '/Documents', component: DocumentsPage },
-  { path: '/Sending', component: SendingPage }
+  { path: '/Sending', component: SendingPage },
+  { path: '/Berichten', component: Berichten }
 ]
 
 const router = new VueRouter({
--- datastreams/src/views/Dashboard.vue
+++ datastreams/src/views/Dashboard.vue
@@ -1,7 +1,153 @@
 <template>
   <Container title="Dashboard">
     <div id="index">
-      
+      <div class="dashboard">
+        <div class="dashboard__component">
+          <h4 class="dashboard__component__title title-blob">
+            Toegevoegde documenten
+          </h4>
+          <div class="dashboard__component__items">
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--green">
+                ju
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Loonstrook Datastreams 04-2020
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--green">
+                ju
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Loonstrook Datastreams 03-2020
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--green">
+                ju
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Loonstrook Datastreams 02-2020
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--blue">
+                ik
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Loonstrook Ikea 01-2020
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--blue">
+                ik
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Loonstrook Ikea 12-2019
+              </div>
+            </div>
+          </div>
+          <router-link to="/Documents">
+            <button class="dashboard__component__action btn-primary">
+                Ga naar mijn documenten
+            </button>
+          </router-link>
+        </div>
+        <div class="dashboard__component">
+          <h4 class="dashboard__component__title title-blob">
+            Bekeken door ontvanger
+          </h4>
+          <div class="dashboard__component__items">
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--blue">
+                gj
+              </div>
+              <div class="dashboard__component__items__item__content">
+                RE: Hypoleen lening aanvragen
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--gray">
+                gj
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Hypoleen heeft bestanden gedownload
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--green">
+                gj
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Hypoleen heeft u een bericht gestuurd
+              </div>
+            </div>
+          </div>
+          <router-link to="/Berichten">
+            <button class="dashboard__component__action btn-primary">
+                Ga naar mijn berichten
+            </button>
+          </router-link>
+        </div>
+        <div class="dashboard__component">
+          <h4 class="dashboard__component__title title-blob">
+            Verwijderde deellinks
+          </h4>
+          <div class="dashboard__component__items">
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--green">
+                ik
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Loonstrook Ikea december
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--blue">
+                sl
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Studielening 2019
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--gray">
+                ju
+              </div>
+              <div class="dashboard__component__items__item__content">
+                Jaaropgave 2019
+              </div>
+            </div>
+            <div class="dashboard__component__items__item">
+              <div class="dashboard__component__items__item__sender dashboard__component__items__item__sender--green">
+                cv
+              </div>
+              <div class="dashboard__component__items__item__content">
+                CV pdf voor sollicitatie
+              </div>
+            </div>
+          </div>
+          <router-link to="/Documents">
+            <button class="dashboard__component__action btn-primary">
+                Ga naar deellinks
+            </button>
+          </router-link>
+        </div>
+        <div class="dashboard__component">
+          <h4 class="dashboard__component__title title-blob">
+            Widget toevoegen
+          </h4>
+          <div class="dashboard__component__add">
+            <img src="../assets/img/AddWidget.png" alt="Toevoegen">
+          </div>
+        </div>
+        <div class="dashboard__component dashboard__component--wide">
+          <h4 class="dashboard__component__title title-blob">
+            Herinneringen
+          </h4>
+        </div>
+      </div>
     </div>
   </Container>
 </template>
