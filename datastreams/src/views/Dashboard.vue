d0cec1b7fa7d76d7c5fed0dd0ac1ccfc2343a4cb
Joshua3011 <j.vangompel@student.fontys.nl>
2020-06-19 11:09:44 +0200

    Reminders setup without calendar

--- datastreams/src/views/Dashboard.vue
+++ datastreams/src/views/Dashboard.vue
@@ -146,6 +146,62 @@
           <h4 class="dashboard__component__title title-blob">
             Herinneringen
           </h4>
+          <div class="dashboard__component__content">
+            <div class="dashboard__component__content__reminders">
+              <table>
+                <tr>
+                  <th>Datum</th>
+                  <th>Opmerking</th>
+                </tr>
+                <tr>
+                  <td>6-4-2020</td>
+                  <td>Loonstrook Ikea</td>
+                  <td>
+                    <img src="../assets/svg/gear.svg" alt="x" class="dashboard__component__reminders__settings">
+                  </td>
+                </tr>
+                <tr>
+                  <td>6-5-2020</td>
+                  <td>Loonstrook Ikea</td>
+                  <td>
+                    <img src="../assets/svg/gear.svg" alt="x" class="dashboard__component__reminders__settings">
+                  </td>
+                </tr>
+                <tr>
+                  <td>20-12-2020</td>
+                  <td>Jaaropgaven Datastreams</td>
+                  <td>
+                    <img src="../assets/svg/gear.svg" alt="x" class="dashboard__component__reminders__settings">
+                  </td>
+                </tr>
+                <tr>
+                  <td>6-6-2020</td>
+                  <td>Loonstrook Ikea</td>
+                  <td>
+                    <img src="../assets/svg/gear.svg" alt="x" class="dashboard__component__reminders__settings">
+                  </td>
+                </tr>
+                <tr>
+                  <td>6-7-2020</td>
+                  <td>Loonstrook Ikea</td>
+                  <td>
+                    <img src="../assets/svg/gear.svg" alt="x" class="dashboard__component__reminders__settings">
+                  </td>
+                </tr>
+                <tr>
+                  <td>6-8-2020</td>
+                  <td>Loonstrook Ikea</td>
+                  <td>X</td>
+                </tr>
+              </table>
+            </div>
+            <div class="dashboard__component__content__calendar">
+              Lorem Ipsum
+            </div>
+          </div>
+          <button class="dashboard__component__action dashboard__component__action--no-center btn-primary">
+              Herinnering toevoegen
+          </button>
         </div>
       </div>
     </div>
