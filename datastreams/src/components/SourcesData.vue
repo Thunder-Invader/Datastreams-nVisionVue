1fbd8dc3845ee7f2335d7194ba18fc5e4081ac1f
Deatlydeath <bas4iphone4@gmail.com>
2020-06-22 16:35:05 +0200

    Source page fixed everything but the icons
    Sources page almost complete, only icons need the right styling and selector. Some scaling issues are known.

---
+++ datastreams/src/components/SourcesData.vue
@@ -0,0 +1,44 @@
+<template>
+    <tr>
+        <td>{{ email }}</td>
+        <td>{{ naam }}</td>
+        <td>{{ documenten }}</td>
+        <td>
+            <img v-if="status=='1'"
+                 src="../assets/svg/gear.svg"
+                 alt="true"
+                 class="sources__icon">
+            <img v-if="status=='0'"
+                 src="../assets/svg/share.svg"
+                 alt="false"
+                 class="sources__icon">
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: "SourcesData",
+        props: {
+            email: String,
+            naam: String,
+            documenten: String,
+            status: String
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import "../scss/_toplevel.scss";
+
+    tr {
+        &:hover,
+        &:focus {
+            background-color: $selected;
+        }
+
+        td {
+            border-bottom: 1px solid $separation-primary;
+        }
+    }
+</style>
---
+++ datastreams/src/demodata/SourceData.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+export const sourcedata = [
+    {
+        email: "t.duivers-sales@ikea.nl",
+        naam: "Sales account",
+        documenten: "Loonstrooken",
+        status: "1",
+    },
+    {
+        email: "thomas.info@ikea.nl",
+        naam: "Info mail",
+        documenten: "Jaaropgaven",
+        status: "1",
+    },
+    {
+        email: "thomas-thuismail@gmail.com",
+        naam: "Thuismail",
+        documenten: "Loonstrooken",
+        status: "1",
+    },
+    {
+        email: "t.duivers@datastreams.nl",
+        naam: "Datastreams mail",
+        documenten: "Rekeningen",
+        status: "1",
+    }
+]
